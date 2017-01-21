54a1ab35df357a5b3b2eee5741556d180013eedf
sergiomgaspar <sergiomgaspar.dev@gmail.com>
2017-01-21 13:41:07 +0100

    final stable release

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,6 +6,7 @@ var logger = require('../common/logger');
 	'use strict';
 	
 	var apiKey = process.env.IMG_API_KEY || 'N/A';
+	var maxResults = 0;
 	
 	// Validation the env variable is a number
 	if (process.env.MAX_RESULTS != undefined && typeof parseInt(process.env.MAX_RESULTS) === 'number') maxResults = process.env.MAX_RESULTS; 
